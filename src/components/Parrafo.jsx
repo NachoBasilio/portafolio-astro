8097895f5c87fd03f6a36e55f4808dc038ec81f8
IgnaDev <arcortoon@gmail.com>
2023-10-22 16:49:29 -0300

    Mejora en la imagen

--- src/components/Parrafo.jsx
+++ src/components/Parrafo.jsx
@@ -14,7 +14,7 @@ export default function Parrafo({ parrafos }) {
       ) : (
         <p className='mt-5 dark:text-zinc-100'>{parrafos[0].substring(0, 150)}...</p>
       )}
-      <button className='text-aguara-600 ' onClick={handleClick}>
+      <button className='text-aguara-600 mt-5' onClick={handleClick}>
         {verMas ? 'Ver menos' : 'Ver más...'}
       </button>
     </div>
