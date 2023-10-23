79d05265e5797c9ffb8e8749e031c9d2f79240ed
IgnaDev <arcortoon@gmail.com>
2023-10-23 16:38:48 -0300

    Estoy furstrado con esto

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -2,18 +2,21 @@ import "../styles/proyectos.css";
 
 export default function Proyecto({ titulo, descripcion, imagen, link }) {
   const divStyle = {
-    backgroundImage: `url(${imagen})`
+    backgroundImage: `url(${imagen})`,
   };
 
   return (
-    <a href={link} target="_blank"  className="flex  mt-5 dark:text-zinc-100 bg-aguara-400 
-    p-2 rounded-lg  m-auto">
-      <div className="titulo-proyecto grow">
-        <h3 className="text-2xl font-bold">{titulo}</h3>
-        <p>{descripcion}</p>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex flex-col mt-5 dark:text-zinc-100 bg-aguara-300 rounded-lg m-auto"
+    >
+      <div className="titulo-proyecto p-2 grow">
+        <h3 className="text-3xl m-auto font-bold">{titulo}</h3>
+        <p className="text-xl mt-2">{descripcion}</p>
       </div>
-      <div className="imagen-proyectos w-28 h-28" style={divStyle}></div>
+      <div className="imagen-proyectos m-auto w-32 h-32" style={divStyle}></div>
     </a>
   );
 }
-
