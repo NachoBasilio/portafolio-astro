3f3db64b4454854da9bfa926b79b197c81da76c3
IgnaDev <arcortoon@gmail.com>
2023-10-23 16:48:11 -0300

    Mejora en las cards, dark

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -10,13 +10,13 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col mt-5 dark:text-zinc-100 bg-aguara-300 rounded-lg m-auto"
+      className="flex flex-col mt-5 dark:text-zinc-100 bg-aguara-300 dark:bg-zinc-900 rounded-lg m-auto"
     >
       <div className="titulo-proyecto grow">
-        <h3 className="text-3xl m-auto pl-2 pt-2 font-bold">{titulo}</h3>
-        <p className="text-xl mt-2 bg-zinc-100 p-2">{descripcion}</p>
+        <h3 className="text-3xl m-auto pl-2 pt-2 font-bold dark:bg-zinc-900">{titulo}</h3>
+        <p className="text-xl mt-2 bg-zinc-100 p-2 dark:bg-zinc-900">{descripcion}</p>
       </div>
-      <div className="imagen-proyectos m-auto bg-zinc-100 h-60" style={divStyle}></div>
+      <div className="imagen-proyectos m-auto bg-zinc-100 h-60 dark:bg-zinc-900" style={divStyle}></div>
     </a>
   );
 }
