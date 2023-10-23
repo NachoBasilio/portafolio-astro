8364851708c38d223755962809c45539a7bd4757
IgnaDev <arcortoon@gmail.com>
2023-10-23 17:02:49 -0300

    Una pequeña mejora

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -10,13 +10,13 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col mt-5 dark:text-zinc-100 bg-aguara-300 dark:bg-zinc-900 rounded-lg m-auto"
+      className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-zinc-900 rounded-2xl m-auto"
     >
       <div className="titulo-proyecto grow">
-        <h3 className="text-3xl m-auto pl-2 pt-2 font-bold dark:bg-zinc-900">{titulo}</h3>
-        <p className="text-xl mt-2 bg-zinc-100 p-2 dark:bg-zinc-900">{descripcion}</p>
+        <h3 className="text-3xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
+        <p className="text-xl mt-2 bg-zinc-100 px-4 py-2 dark:bg-zinc-900 tracking-wider">{descripcion}</p>
       </div>
-      <div className="imagen-proyectos m-auto bg-zinc-100 h-60 dark:bg-zinc-900" style={divStyle}></div>
+      <div className="imagen-proyectos m-auto bg-zinc-100 h-60 dark:bg-aguara-1000" style={divStyle}></div>
     </a>
   );
 }
