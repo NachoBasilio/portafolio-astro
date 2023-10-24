b6f752f65db4d3784a779156085fa8d9def33bba
IgnaDev <arcortoon@gmail.com>
2023-10-24 20:24:45 -0300

    Sigue medio raro pero mejor

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -12,7 +12,7 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
       rel="noopener noreferrer"
       className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
     >
-      <div className="titulo-proyecto grow h-48 hover:h-auto">
+      <div className="titulo-proyecto grow md:h-48 hover:h-auto">
         <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
         <p className="text-xl mt-2 bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
       </div>
