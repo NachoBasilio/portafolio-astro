1577f6ae1e4a76e7d19ec52d62e4508f0a8338d0
IgnaDev <arcortoon@gmail.com>
2023-10-24 20:17:56 -0300

    Mejora de las cards

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -12,11 +12,12 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
       rel="noopener noreferrer"
       className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
     >
-      <div className="titulo-proyecto grow">
+      <div className="titulo-proyecto grow h-48 hover:h-auto">
         <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
-        <p className="text-xl mt-2 bg-zinc-100 px-4 py-2 dark:bg-zinc-800  tracking-wider">{descripcion}</p>
+        <p className="text-xl mt-2 bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
       </div>
       <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
     </a>
   );
+En 
 }
