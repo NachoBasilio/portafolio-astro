b631aff78da27c1bec21d27b52b7f2ffbe2210be
IgnaDev <arcortoon@gmail.com>
2023-10-24 20:47:14 -0300

    Sigue medio raro pero mejor

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -6,18 +6,19 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
   };
 
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
-    >
-      <div className="titulo-proyecto grow md:h-48 hover:h-auto">
-        <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
-        <p className="text-xl mt-2 bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
-      </div>
-      <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
-    </a>
+<a
+  href={link}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
+>
+  <div className="titulo-proyecto grow  overflow-hidden transition-all duration-300">
+    <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
+    <p className="text-xl mt-2 hover:h-auto h-52 bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
+  </div>
+  <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
+</a>
+
   );
 En 
 }
