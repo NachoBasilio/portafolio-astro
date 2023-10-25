b6393553c7f1e621a75b76b5593fd630b5d29998
IgnaDev <arcortoon@gmail.com>
2023-10-25 09:34:01 -0300

    cambios

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -12,9 +12,9 @@ export default function Proyecto({ titulo, descripcion, imagen, link }) {
   rel="noopener noreferrer"
   className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
 >
-  <div className="titulo-proyecto grow  overflow-hidden transition-all duration-300">
+  <div className="titulo-proyecto grow transition-all duration-300">
     <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
-    <p className="text-xl mt-2 hover:h-auto h-52 bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
+    <p className="text-xl mt-2 h-52 overflow-hidden hover:overflow-y-scroll bg-zinc-100 px-4 py-2 dark:bg-zinc-800 tracking-wider">{descripcion}</p>
   </div>
   <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
 </a>
