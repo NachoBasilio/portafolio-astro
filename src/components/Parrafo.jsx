f5a8930ec4415144c28cf6439ff1f47d66f09db4
IgnaDev <arcortoon@gmail.com>
2023-10-26 19:05:12 -0300

    Header mejorado

--- src/components/Parrafo.jsx
+++ src/components/Parrafo.jsx
@@ -12,7 +12,7 @@ export default function Parrafo({ parrafos }) {
       {verMas ? (
         parrafos.map((parrafos, index) => <p className='mt-5 dark:text-zinc-100 tracking-wide text-xl' key={index}>{parrafos}</p>)
       ) : (
-        <p className='mt-5 dark:text-zinc-100 tracking-wide text-xl'>{parrafos[0].substring(0, 150)}...</p>
+        <p className='mt-5 dark:text-zinc-100 tracking-wide text-xl'>{parrafos[0].substring(0, 600)}...</p>
       )}
       <button className='text-aguara-600 mt-5 tracking-wide' onClick={handleClick}>
         {verMas ? 'Ver menos' : 'Ver más...'}
