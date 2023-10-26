27b82aeba186f2b50f473bde54fdbe610ac6f607
IgnaDev <arcortoon@gmail.com>
2023-10-26 20:06:36 -0300

    Hasta aca llegue mejorando las cards, despues sigo

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,23 +1,34 @@
 import "../styles/proyectos.css";
 
-export default function Proyecto({ titulo, descripcion, imagen, link }) {
+export default function Proyecto({ titulo, imagen, link }) {
   const divStyle = {
     backgroundImage: `url(${imagen})`,
   };
 
-  return (
-<a
-  href={link}
-  target="_blank"
-  rel="noopener noreferrer"
-  className="flex flex-col mt-5 border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-2xl m-auto"
->
-  <div className="titulo-proyecto grow transition-all duration-300">
-    <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
-  </div>
-  <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
-</a>
+  const grid = ()=>{
+    if(titulo == "Rubrica Pokemon"){
+      return "md:col-span-4 md:row-span-4"
+    }else if(titulo == "Galeria de Imagenes"){
+      return "md:col-span-6"
+    }else{
+      return "md:col-span-2"
+    }
+  }
 
+  return (
+    <div className={grid(titulo) + " mt-10 md:m-0"} >
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex flex-col border-aguara-700 dark:text-zinc-100 bg-aguara-300 dark:bg-aguara-900 rounded-xl m-auto"
+      >
+      <div className="titulo-proyecto grow">
+        <h3 className="text-xl m-auto pl-4 pt-2 font-bold tracking-wider">{titulo}</h3>
+      </div>
+      <div className="imagen-proyectos m-auto rounded-b bg-zinc-100 h-60 dark:bg-zinc-800" style={divStyle}></div>
+      </a>
+    </div>
   );
 En 
 }
